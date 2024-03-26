90789b70b86a2c443b4becc19072f9c75c932c8d
Ashwanth S <appleashwanth2004@gmail.com>
2024-03-26 19:50:27 +0530

    initial commit

---
+++ app/src/main/java/com/example/librarybookdueremainder/AdminRemove.java
@@ -0,0 +1,14 @@
+package com.example.librarybookdueremainder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminRemove extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_remove);
+    }
+}
